eb44ddefd4a1bb38cec41de347e53d2f5a308171
BeauBryanDev <bryan.beau.186@gmail.com>
2025-09-01 12:10:43 -0500

    set first sub dir for basic php web understanding project structure

---
+++ php4web/blog.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blogs</title>
+</head>
+<body>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="./aboutMe.php">About</a></li>
+        <li><a href="./contact.php">Contact</a></li>
+        <li><a href="./blog.php">Blog</a></li>
+        <li><a href="./projects.php">Projects</a></li>
+    </ul>
+    <h1>
+        Welcome to my website
+    </h1>
+    <h2>this is my blog page</h2>
+</body>
+</html>
